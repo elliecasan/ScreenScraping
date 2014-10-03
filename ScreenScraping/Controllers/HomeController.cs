84e65be6e7785775a02ae58dae79e7901de38246
elliecasan <ellie@ewerlow.se>
2014-10-03 10:22:44 +0200

    Bra kod

--- ScreenScraping/Controllers/HomeController.cs
+++ ScreenScraping/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using ScreenScraping.Models.Home;
 
 namespace ScreenScraping.Controllers
 {
@@ -11,9 +15,38 @@ namespace ScreenScraping.Controllers
         //
         // GET: /Home/
 
-        public ActionResult Index()
+        public ActionResult Index(string message)
         {
-            return View();
+            var viewModel = new IndexViewModel();
+
+            if (!string.IsNullOrEmpty(message))
+                viewModel.CompanyName = message;
+            
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            
+            string url = "http://www.allabolag.se/" + model.ORGNumber;
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string result = reader.ReadToEnd();
+            stream.Dispose();
+            reader.Dispose();
+
+            int firstChar = result.IndexOf("reportTitleBig")+16;
+            string Name = result.Substring(firstChar, 100);
+            int lastChar = Name.IndexOf("</span>");
+            Name = Name.Substring(0, lastChar);
+
+            return RedirectToAction("Index", "Home", new {message = Name});
         }
 
     }
---
+++ ScreenScraping/Models/Home/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScreenScraping.Models.Home
+{
+    public class IndexViewModel
+    {
+        [Required(ErrorMessage = "Enter correct value")]
+        public string ORGNumber{ get; set; }
+        public string CompanyName { get; set; }
+    }
+}
