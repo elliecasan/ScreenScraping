5481c33e5802ae05446ad51741d255ebeec66dd0
elliecasan <ellie@ewerlow.se>
2014-10-06 10:21:58 +0200

    Meeeen

--- ScreenScrapingLib/Services/HittaSeScreenScraperService.cs
+++ ScreenScrapingLib/Services/HittaSeScreenScraperService.cs
@@ -6,29 +6,42 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using ScreenScrapingLib.Services;
+using HtmlAgilityPack;
+
 
 namespace ScreenScrapingLib.Services
 {
+    
     public class HittaSeScreenScraperService : IScreenScraperService
     {
         public string GetCompanyNameByOrgNr(long orgNr)
         {
-            string url = "http://www.hitta.se/sök?vad=" + orgNr;
-            url = HttpUtility.UrlEncode(url);
-            WebRequest request = WebRequest.Create(url);
-            WebResponse response = request.GetResponse();
-            Stream stream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(stream);
-            string result = reader.ReadToEnd();
-            stream.Dispose();
-            reader.Dispose();
+            
+            
+            HtmlWeb htmlweb= new HtmlWeb();
 
-            int firstChar = result.IndexOf("legalname\">");
-            string Name = result.Substring(firstChar);
-            int lastChar = Name.IndexOf("</h2>");
-            Name = Name.Substring(0, lastChar);
+            var url = htmlweb.Load("http://www.hitta.se/sök?vad="+ orgNr);
 
-            return Name;
+            //HtmlDocument doc = htmlweb.Load(url);
+
+            //string url = "http://www.hitta.se/sök?vad=" + orgNr;
+
+            //url = HttpUtility.UrlEncode(url);
+            //WebRequest request = WebRequest.Create(url);
+            //WebResponse response = request.GetResponse();
+            //Stream stream = response.GetResponseStream();
+            //StreamReader reader = new StreamReader(stream);
+            //string result = reader.ReadToEnd();
+            //stream.Dispose();
+            //reader.Dispose();
+
+            //int firstChar = result.IndexOf("legalname\">");
+            //string Name = result.Substring(firstChar);
+            //int lastChar = Name.IndexOf("</h2>");
+            //Name = Name.Substring(0, lastChar);
+
+            return null;
         }
     }
 }
